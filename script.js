3cb945e887fadf4eba25e7111f6c638618c3198a
hehenischal <nischallc56@gmail.com>
2023-08-20 09:49:22 +0545

    final commit: 10 mins late

--- script.js
+++ script.js
@@ -1,43 +1,65 @@
-const accidentCountElement = document.getElementById('accidentCount');
-const injuryPercentageElement = document.getElementById('injuryPercentage');
-const fatalPercentageElement = document.getElementById('fatalPercentage');
+// const accidentCountElement = document.getElementById('accidentCount');
+// const injuryPercentageElement = document.getElementById('injuryPercentage');
+// const fatalPercentageElement = document.getElementById('fatalPercentage');
 
 
 let totalAccidents;
 // Update the accident count and statistics
-function updateStatistics() {
-    const startDate = new Date('2023-08-19'); // Change this to your desired start date
-    const currentDate = new Date();
-    const daysDifference = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24));
-    const accidentsPerDay = 7;
+// function updateStatistics() {
+//     const startDate = new Date('2023-08-19'); // Change this to your desired start date
+//     const currentDate = new Date();
+//     const daysDifference = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24));
+//     const accidentsPerDay = 7;
 
-    totalAccidents = accidentsPerDay * daysDifference;
-    const injuryPercentage = 72;
-    const fatalPercentage = 7;
+//     totalAccidents = accidentsPerDay * daysDifference;
+//     const injuryPercentage = 72;
+//     const fatalPercentage = 7;
 
-    accidentCountElement.textContent = totalAccidents;
-    injuryPercentageElement.textContent = `${injuryPercentage}%`;
-    fatalPercentageElement.textContent = `${fatalPercentage}%`;
-}
+//     accidentCountElement.textContent = totalAccidents;
+//     injuryPercentageElement.textContent = `${injuryPercentage}%`;
+//     fatalPercentageElement.textContent = `${fatalPercentage}%`;
+// }
 
 // Call the updateStatistics function to populate data on page load
-updateStatistics();
+// updateStatistics();
+
+// Function to animate counting from 0 to a target number
 
 
-const countElement = document.getElementById('count');
+const startDate = new Date('2023-08-19'); // Change this to your desired start date
+const currentDate = new Date();
+const daysDifference = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24));
+const accidentsPerDay = 7;
+totalAccidents = accidentsPerDay * daysDifference;
+const injuryPercentage = 72;
+const fatalPercentage = 7;
+const totalDeaths = Math.ceil(fatalPercentage/100 * totalAccidents);
+const totalInjuries = Math.ceil((injuryPercentage/100 * totalAccidents) - totalDeaths);
+//cieling function
 
-function startCounting(targetCount) {
-    let currentCount = 0;
-    const increment = Math.ceil(targetCount / 100); // Adjust as needed
-    const interval = setInterval(() => {
-        currentCount += increment;
-        countElement.textContent = currentCount.toLocaleString(); // Add commas for readability
 
-        if (currentCount >= targetCount) {
+
+function animateCount(targetNumber, elementId) {
+    const countElement = document.getElementById(elementId);
+
+    let currentNumber = 0;
+    const increment = Math.ceil(targetNumber / 100); // Adjust as needed
+    console.log(increment);
+    function updateCount() {
+        currentNumber += increment;
+        if (currentNumber >= targetNumber) {
+            currentNumber = targetNumber;
             clearInterval(interval);
         }
-    }, 20); 
+
+        countElement.textContent = currentNumber.toLocaleString(); // Add commas for readability
+    }
+
+    const interval = setInterval(updateCount, 20); // Adjust the interval for desired speed
+    console.log(interval);
 }
 
-startCounting( 100 );
-
+// Call the animateCount function with the target numbers and element IDs
+animateCount(totalAccidents, 'count'); // Change 500 to your desired target number
+animateCount(totalInjuries, 'injury'); // Change 300 to your desired target number for injuries
+animateCount(totalDeaths, 'death'); // Change 50 to your desired target number for deaths
